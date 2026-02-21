3d853e57cf6a3631e7a25f499d2ca0ee60d04cde
ksm1l <kamildbona15@gmail.com>
2026-02-21 06:07:35 -0600

    shows distance from the quad in console

--- test.js
+++ test.js
@@ -2,19 +2,47 @@ const startBtn = document.getElementById("start");
 const pinDrop = document.getElementById("mapbtn");
 const sendBtn = document.getElementById("send");
 
+// constants of the limits of our picture in real coordinates
+const northLimit = 49.813089;
+const southLimit = 49.804333;
+const westLimit = -97.142139;
+const eastLimit = -97.128248;
+
+// starts
 startBtn.addEventListener("click", killDiv);
+
+// gives location of click within the map
 pinDrop.addEventListener("click", function (e) {
   getPos(e)
+
 });
+
+// will be used to into the next picture
 sendBtn.addEventListener("click", changeImage);
 
+// transforms map coord into pixel coord
+function transformX(coordWE) {
+  let base1 = 1 - (-coordWE + eastLimit) / (-westLimit + eastLimit)
+  return base1 * 1300;
+}
 
+// transforms map coord into pixel coord
+function transformY(coordNS) {
+  let base1 = 1 - (coordNS - southLimit) / (northLimit - southLimit)
+  return base1 * 1241;
+}
+
+// gets click position and calculates distance from set target
 function getPos(e) {
   xCoordsUser = e.offsetX;
   yCoordsUser = e.offsetY;
-  console.log(xCoordsUser + ' ' + yCoordsUser);
+  let result = distanceCalc(xCoordsUser, yCoordsUser, transformX(xCoords[0]), transformY(yCoords[0]));
+  console.log('distance from the quad is ' + result + ' pixels');
+  console.log(transformX(xCoords[0]));
+  console.log(transformY(yCoords[0]));
 }
 
+// starts 
 function killDiv() {
   console.log('currImg');
   document.getElementById("menu").style.display = "none";
@@ -23,6 +51,7 @@ function killDiv() {
   document.getElementById("mapSide").style.display = "flex";
 }
 
+// rotates through an array of images
 function changeImage() {
   if (currImg < 5) {
 
@@ -35,11 +64,12 @@ function changeImage() {
   }
 }
 
-var currImg = 0;
 
 
-const xCoords = [];
-const yCoords = [];
+
+//first coords are the quad
+const xCoords = [-97.132071];
+const yCoords = [49.808769];
 const url = ["url('https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Pink_lady_and_cross_section.jpg/1920px-Pink_lady_and_cross_section.jpg')"
   , "url('https://raw.githubusercontent.com/ksm1l/test/refs/heads/main/image1.png')"
   , "url('https://raw.githubusercontent.com/ksm1l/test/refs/heads/main/image2.png')"
@@ -47,13 +77,15 @@ const url = ["url('https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/Pin
   , "url('https://raw.githubusercontent.com/ksm1l/test/refs/heads/main/image4.png')"
 ];
 
+var currImg = 0;
 var xCoordsUser = 0;
 var yCoordsUser = 0;
 
-let points = pointsCalc(21);
-console.log(points);
+function distanceCalc(x1, y1, x2, y2) {
+  const dx = x2 - x1;
+  const dy = y2 - y1;
 
-function distanceCalc(x, y, x2, y2) {
+
   let rad = 6371;
 
   let xDegLat = x * (Math.PI / 180);
@@ -75,13 +107,14 @@ function distanceCalc(x, y, x2, y2) {
   let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   let distanceInKm = rad * c;
 
-  let distanceInM = distanceInKm / 1000;
+  let distanceInM = distanceInKm;
 
-  return distanceInM;
+  return Math.sqrt(dx * dx + dy * dy);
 }
 
 //let calcDistance = distanceCalc();
 
+// not updated
 function pointsCalc(distance) {
   let points = 100;
   let maxDistance = 500;
